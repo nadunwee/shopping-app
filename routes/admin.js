51dd648fda495e8339219dc1098ac44f434aa3f0
nadun weerakkody <weerakkody.kn@gmail.com>
2023-12-19 09:31:49 +0530

    initial commit

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/products' method='POST'><input type='text' name='title'><button>Add Product</button></form>"
+  );
+});
+
+router.post("/products", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+module.exports = router;
