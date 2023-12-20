5389e1dd86014bf14964a2637bbccf1ed78e4eec
nadun weerakkody <weerakkody.kn@gmail.com>
2023-12-20 21:02:17 +0530

    add MVC framework

--- routes/admin.js
+++ routes/admin.js
@@ -2,22 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const products = [];
+const productsController = require("../controllers/products");
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "add product",
-    path: "/admin/add-product",
-    formCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
-router.post("/products", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/products", productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,10 @@
 const path = require("path");
 const express = require("express");
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const routes = express.Router();
 
-routes.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    path: "/",
-    pageTitle: "Shop",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+routes.get("/", productsController.getProducts);
 
 module.exports = routes;
