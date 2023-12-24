881ea8c1758fee8312b934a9e6840e3bd4e6a202
nadun weerakkody <weerakkody.kn@gmail.com>
2023-12-25 05:23:32 +0530

    finish product

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
 
-const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 app.listen(3000);
-
-// mongoConnect((client) => {
-//   console.log(client);
-// });
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,25 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
 
-const shopController = require("../controllers/shop");
+const express = require('express');
 
-const routes = express.Router();
+const shopController = require('../controllers/shop');
 
-routes.get("/", shopController.getIndex);
+const router = express.Router();
 
-routes.get("/products", shopController.getProducts);
+router.get('/', shopController.getIndex);
 
-routes.get("/products/:productID", shopController.getProduct);
+router.get('/products', shopController.getProducts);
 
-routes.get("/cart", shopController.getCart);
+router.get('/products/:productId', shopController.getProduct);
 
-routes.post("/cart", shopController.postCart);
+router.get('/cart', shopController.getCart);
 
-routes.get("/checkout", shopController.getCheckout);
+router.post('/cart', shopController.postCart);
 
-routes.get("/orders", shopController.getOrders);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-module.exports = routes;
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
